600ffd235a1d888c58fba71ae4b83c345c808553
Yutae11 <yutaokkotsu1110@gmail.com>
2024-05-17 01:52:40 +0530

    Update support.js

--- src/handlers/helppanel/support.js
+++ src/handlers/helppanel/support.js
@@ -17,25 +17,25 @@ module.exports = async (client) => {
                                 {
                                     label: `Commands`,
                                     description: `Show the commands of ${client.user.username}!`,
-                                    emoji: "💻",
+                                    emoji: "<:Commands:1240401083914780743>",
                                     value: "commands-Bothelp",
                                 },
                                 {
                                     label: `Invite`,
                                     description: `Invite ${client.user.username} to your server`,
-                                    emoji: "📨",
+                                    emoji: "<:invitesss:1240749167530348554>",
                                     value: "invite-Bothelp",
                                 },
                                 {
                                     label: `Support server`,
                                     description: `Join the suppport server`,
-                                    emoji: "❓",
+                                    emoji: "<:Support:1239465172524077087>",
                                     value: "support-Bothelp",
                                 },
                                 {
                                     label: `Changelogs`,
                                     description: `Show the ${client.user.username} changelogs`,
-                                    emoji: "📃",
+                                    emoji: "<:Commands:1240401083914780743>",
                                     value: "changelogs-Bothelp",
                                 },
                             ]),
@@ -52,7 +52,7 @@ module.exports = async (client) => {
                 client.embed({
                     title: `❓・Support`,
                     desc: `Make your server even better with ${client.user.username}!`,
-                    image: "https://cdn.discordapp.com/attachments/843487478881976381/874694194474668052/Bot_banner_invite.jpg",
+                    image: "https://cdn.discordapp.com/attachments/1235849726780379148/1240750173500608563/d601713de8002b9be1d0c1d72b60c759.jpg?ex=6647b225&is=664660a5&hm=614651533ddd22713c354e819b79e6b6c5125f5d8ba829af4349e8e4d71130ac&",
                     url: client.config.discord.serverInvite,
                     components: [row2, row],
                     type: 'edit'
